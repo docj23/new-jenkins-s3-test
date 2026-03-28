01b7855cde4b7d8ea4355e7a88188d3fd91bb604
docj23 <lshelton32@yahoo.com>
2026-03-28 11:11:36 -0500

    change made to bucket to mine own

--- test-bucket.tf
+++ test-bucket.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "terraform-state-aaronmcd"                 # Name of the S3 bucket
+    bucket  = "cigarhubsec"                 # Name of the S3 bucket
     key     = "jenkins-test-031726.tfstate"        # The name of the state file in the bucket
     region  = "us-east-2"                          # Use a variable for the region
     encrypt = true                                 # Enable server-side encryption (optional but recommended)
